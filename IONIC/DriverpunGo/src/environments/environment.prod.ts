f8e931c0dbca10358de8c4bc83d98975a5248f08
Jesen Louis <jesenagustian6@gmail.com>
2026-06-26 21:15:14 +0700

    chore: update production API URL and rebuild customer app

--- IONIC/DriverpunGo/src/environments/environment.prod.ts
+++ IONIC/DriverpunGo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api',
+  apiUrl: 'https://api.ambatu.my.id/api',
   mapboxToken: 'pk.YOUR_MAPBOX_TOKEN_GOES_HERE',
   pusherKey: 'de107372da398508e2c5',
   pusherCluster: 'ap1'
--- IONIC/DriverpunGo/src/environments/environment.ts
+++ IONIC/DriverpunGo/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api',
+  apiUrl: 'https://api.ambatu.my.id/api',
   mapboxToken: 'pk.YOUR_MAPBOX_TOKEN_GOES_HERE',
   pusherKey: 'de107372da398508e2c5',
   pusherCluster: 'ap1'
