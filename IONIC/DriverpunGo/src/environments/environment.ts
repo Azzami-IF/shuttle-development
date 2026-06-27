e649088de63530e7b72f5b221d4f7e425b8897bc
Azzami-IF <habiburrazami@gmail.com>
2026-06-27 07:20:16 +0700

    feat: update environment configuration with new Mapbox token and add IDE configuration files

--- IONIC/DriverpunGo/src/environments/environment.ts
+++ IONIC/DriverpunGo/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api',
-  mapboxToken: 'pk.YOUR_MAPBOX_TOKEN_GOES_HERE',
+  mapboxToken: 'pk.eyJ1IjoiYXp6YW1pIiwiYSI6ImNtcW5wN3p0NTAwYTYycXE0dTEzYjl1cWcifQ.-oRDhG4TcRatMl2CWKfvFg',
   pusherKey: 'de107372da398508e2c5',
   pusherCluster: 'ap1'
 };
